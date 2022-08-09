2af7d7cdad2939e558dedcf8a35592f983221844
Daryna <condordaryna@gmail.com>
2022-08-09 20:08:53 +0300

    fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+let city = prompt("Your city:");
 function weather () {
-    let city = prompt("Your city:");
     fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`)
     .then ((res) => res.json())
     .then ((data) => {
